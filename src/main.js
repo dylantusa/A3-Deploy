8d3227a3284795c299a11211428caca5cfbe6976
dylantusa <80488472+dylantusa@users.noreply.github.com>
2022-10-20 22:00:13 -0500

    Commit

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-createApp(App).use(router).mount('#app')
