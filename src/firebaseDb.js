ffc6d5270debb235c868251c32fcdb865d8544aa
dylantusa <80488472+dylantusa@users.noreply.github.com>
2022-10-22 14:31:25 -0500

    commit

--- src/firebaseDb.js
+++ src/firebaseDb.js
@@ -1,14 +1,14 @@
-import firebase from "firebase/app"; 
-import "firebase/firestore"; 
+import * as firebase from 'firebase';
 
-const firebaseConfig = { 
-    apiKey: 'AIzaSyAxFUcfL9GINRhetdcBsVLpKsiJhZFr4ro', 
-    authDomain: 'my-subscriptions-dee08.firebaseapp.com', 
-    projectId: 'my-subscriptions-dee08', 
-    storageBucket: 'my-subscriptions-dee08.appspot.com', 
-    messagingSenderId: '650006726814', 
-    appId: '1:650006726814:web:7e5ca5201c235a31e1d38f' 
-}
+const firebaseConfig = {
+    apiKey: "AIzaSyAxFUcfL9GINRhetdcBsVLpKsiJhZFr4ro",
+    authDomain: "my-subscriptions-dee08.firebaseapp.com",
+    projectId: "my-subscriptions-dee08",
+    storageBucket: "my-subscriptions-dee08.appspot.com",
+    messagingSenderId: "650006726814",
+    appId: "1:650006726814:web:7e5ca5201c235a31e1d38f"
+  };
 
-const db = firebase.initializeApp(firebaseConfig); 
-export default db.firestore();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const db = firebaseApp.firestore();
